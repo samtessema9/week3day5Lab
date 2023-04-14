39c248b0fe94ece7029a716ba7ae2118d636be46
Jonathan Pena-Loa <jpena-loa@LABA.PSWAS.LOCAL>
2023-04-14 11:45:30 -0700

    debuging

--- function.js
+++ function.js
@@ -63,6 +63,8 @@ const maxOfthree = (num1, num2, num3) => {
 console.log(maxOfthree(6, 9, 1));
 
 
+
+
 const printLongestWord = (wordArray) => {
     console.log(wordArray.sort((a, b) => b.length - a.length)[0]);
 }
