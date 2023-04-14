f031102e6157ff319c944ae2ca0a32428bd7b895
nonwor <nonwor@gmail.com>
2023-04-14 13:46:27 -0700

    done

--- Extra/extra_credit.js
+++ Extra/extra_credit.js
@@ -1,4 +1,4 @@
-//Extra Credit
+//Extra Credit- Sataporn Worasilpchai
 //Cat Combinator
 
 const cat1 = {
@@ -16,3 +16,19 @@ const cat2 = {
     age: 9
 }
 
+const combineCats = (obj1, obj2) =>{
+
+    console.log([{...obj1},{...obj2}])
+
+
+    let babyCat = {};
+    babyCat.name = obj1.name + obj2.name;
+    babyCat.age = 1;
+    babyCat.breed = obj1.breed + "-" + obj2.breed;
+    return babyCat
+}
+
+let newCat = combineCats(cat1, cat2);
+console.table(newCat);
+
+console.log(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)));
